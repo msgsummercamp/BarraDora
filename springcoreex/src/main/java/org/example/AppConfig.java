11b4fef0414e0fcd3de4a43c557b8b1e2e6708de
dorabarra <dorabarra2004@gmail.com>
2025-07-16 15:30:01 +0300

    Dependency Injection exercise

---
+++ springcoreex/src/main/java/org/example/AppConfig.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public HelloBean helloBean() {
+        return new HelloBean();
+    }
+}
--- springcoreex/src/main/java/org/example/Main.java
+++ springcoreex/src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        HelloBean helloBean = (HelloBean) context.getBean("helloBean");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloBean helloBean = context.getBean(HelloBean.class);
         helloBean.sayHello();
     }
 }
