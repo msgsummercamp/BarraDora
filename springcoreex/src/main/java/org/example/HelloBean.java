0aa5f1e4c6439111de03a8c551f7952ec6509596
dorabarra <dorabarra2004@gmail.com>
2025-07-16 15:25:28 +0300

    Added exercises 1-5 from Spring modules section

---
+++ springcoreex/src/main/java/org/example/HelloBean.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class HelloBean {
+    public void sayHello() {
+        System.out.println("Hello, Spring!");
+    }
+}
---
+++ springcoreex/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloBean helloBean = (HelloBean) context.getBean("helloBean");
+        helloBean.sayHello();
+    }
+}
