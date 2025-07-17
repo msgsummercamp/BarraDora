06f584ffe0ac3acaf5483269fcd5fbaa17955f3c
dorabarra <dorabarra2004@gmail.com>
2025-07-17 11:58:27 +0300

    Added logging

--- src/main/java/com/example/spring_boot_ex/controller/UserController.java
+++ src/main/java/com/example/spring_boot_ex/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.spring_boot_ex.controller;
 
 import com.example.spring_boot_ex.model.UserModel;
 import com.example.spring_boot_ex.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,22 +13,34 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
     @GetMapping("/{id}")
     public ResponseEntity<UserModel> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+        logger.info("GET /users/{} called", id);
+        UserModel usermodel = userService.getUserById(id);
+        logger.info("User with id {} retrieved successfully", id);
+        return ResponseEntity.ok(usermodel);
     }
 
     @GetMapping
     public ResponseEntity<List<UserModel>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+        logger.info("GET /users called");
+        List<UserModel> users = userService.getAllUsers();
+        logger.info("List of users retrieved succesfully");
+        logger.info("Returned {} users", users.size());
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping
     public ResponseEntity<UserModel> createUser(@RequestBody UserModel usermodel) {
-        return ResponseEntity.ok(userService.createUser(usermodel));
+        logger.info("POST /users called with user: {}", usermodel);
+        UserModel createdUser = userService.createUser(usermodel);
+        logger.info("User created with ID: {}", createdUser.getId());
+        return ResponseEntity.ok(createdUser);
     }
+
 }
--- src/main/java/com/example/spring_boot_ex/service/UserService.java
+++ src/main/java/com/example/spring_boot_ex/service/UserService.java
@@ -2,6 +2,8 @@ package com.example.spring_boot_ex.service;
 
 import com.example.spring_boot_ex.model.UserModel;
 import com.example.spring_boot_ex.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,20 +11,24 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public UserModel getUserById(Long id) {
+        logger.info("Getting user with id {}", id);
         return userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found with id " + id));
     }
     public List<UserModel> getAllUsers() {
+        logger.info("Getting all users");
         return userRepository.findAll();
     }
 
     public UserModel createUser(UserModel user) {
+        logger.info("Creating a new user");
         return userRepository.save(user);
     }
 }
