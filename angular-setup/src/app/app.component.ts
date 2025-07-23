516959e785dcce81531b1a74f5b2377c0828edb0
dorabarra <dorabarra2004@gmail.com>
2025-07-23 13:06:12 +0300

    Added navbar

--- angular-setup/src/app/app.component.ts
+++ angular-setup/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatToolbar, MatIcon],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-setup';
