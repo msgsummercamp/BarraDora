0a6921e4d7ee0119c07748d316b105b72ebbe30d
dorabarra <dorabarra2004@gmail.com>
2025-07-23 15:55:19 +0300

    Clean review correction 2

---
+++ angular-setup/.angular/cache/19.2.15/angular-setup/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-W5JRI35A.js";
+import "./chunk-VGBVI4OJ.js";
+import "./chunk-7HE7GMPI.js";
+import "./chunk-5D2TMVG5.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
