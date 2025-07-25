313b41f6ba6f855ed77eb09eecc673b0648069ec
dorabarra <dorabarra2004@gmail.com>
2025-07-25 10:57:59 +0300

    Angular Routing

--- frontend/angular/src/login/login.component.ts
+++ frontend/angular/src/login/login.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-login',
     template: `<p>Login works!</p>`,
-    styleUrls: ['./login.component.scss']
+    styleUrl: './login.component.scss',
 })
 export class LoginComponent {}
